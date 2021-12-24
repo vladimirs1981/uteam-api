dc40b32077efa16919ed9636caed65587b49b346
vladimirs1981 <st.vladas@hotmail.com>
2021-12-24 14:25:07 +0100

    eslint, sequelize, user model

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,10 +4,14 @@ dotenv.config();
 
 const SERVER_HOSTNAME: string = process.env.SERVER_HOSTNAME || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME || 'root';
 
 const SERVER = {
 	hostname: SERVER_HOSTNAME,
 	port: SERVER_PORT,
+	db_pass: DB_PASSWORD,
+	db_name: DB_NAME,
 };
 
 const config = {
---
+++ src/models/user.ts
@@ -0,0 +1,47 @@
+import { Sequelize, Model, DataTypes } from 'sequelize';
+
+import config from '../config/config';
+
+const sequelize = new Sequelize(
+	`mysql://${config.server.db_name}:${config.server.db_pass}@${config.server.hostname}:3306/uteam`
+);
+
+interface UserAttributes {
+	id: number;
+	username: string;
+	email: string;
+	password: string;
+}
+
+class User extends Model<UserAttributes> implements UserAttributes {
+	public id!: number;
+	public username!: string;
+	public email!: string;
+	public password!: string;
+}
+
+User.init(
+	{
+		id: {
+			type: DataTypes.INTEGER.UNSIGNED,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		username: {
+			type: new DataTypes.STRING(128),
+			allowNull: false,
+		},
+		email: {
+			type: new DataTypes.STRING(128),
+			allowNull: false,
+		},
+		password: {
+			type: new DataTypes.STRING(128),
+			allowNull: false,
+		},
+	},
+	{
+		tableName: 'users',
+		sequelize,
+	}
+);
